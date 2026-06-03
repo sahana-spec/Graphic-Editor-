71e716488f0ad448f652065e50a330b36da033da
sahana-spec <sahanasuresh1402@gmail.com>
2026-06-03 22:25:50 +0530

    Implement picture drawing functions in main.c
    Added functions to clear and display a picture.

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define WIDTH 80
+#define HEIGHT 24
+
+#define EMPTY '_'
+#define PIXEL '*'
+
+char picture[HEIGHT][WIDTH];
+void clearPicture() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            picture[i][j] = EMPTY;
+        }
+    }
+}
+
+void displayPicture() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            printf("%c", picture[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void setPixel(int x, int y) {
+    if(x >= 0 && x < WIDTH &&
+       y >= 0 && y < HEIGHT) {
+        picture[y][x] = PIXEL;
+    }
+}
